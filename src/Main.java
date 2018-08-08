b0443d2327333e780af41eefde83d70dbb53f4b4
SmithaRS <--smitharavi713@gmail.com>
2018-08-08 17:44:09 -0500

    needs to be tested

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,131 @@ public class Main {
     public static void main() {
         Scanner input = new Scanner(System.in);
         String word = input.nextLine();
+
         WordFinder user = new WordFinder(word, "words_alpha.txt");
+        for (int i = 0; i < word.length() - 1; i++) {
+            char temp = word.charAt(i);
+            switch (temp) {
+
+                case 'a':
+                    break;
+                case 'b':
+                    break;
+                case 'c':
+                    break;
+                case 'd':
+                    break;
+                case 'e':
+                    break;
+                case 'f':
+                    break;
+                case 'g':
+                    break;
+                case 'h':
+                    break;
+                case 'i':
+                    break;
+                case 'j':
+                    break;
+                case 'k':
+                    break;
+                case 'l':
+                    break;
+                case 'm':
+                    break;
+                case 'n':
+                    break;
+                case 'o':
+                    break;
+                case 'p':
+                    break;
+                case 'q':
+                    break;
+                case 'r':
+                    break;
+                case 's':
+                    break;
+                case 't':
+                    break;
+                case 'u':
+                    break;
+                case 'v':
+                    break;
+                case 'w':
+                    break;
+                case 'x':
+                    break;
+                case 'y':
+                    break;
+                case 'z':
+                    break;
+                case 'A':
+                    break;
+                case 'B':
+                    break;
+                case 'C':
+                    break;
+                case 'D':
+                    break;
+                case 'E':
+                    break;
+                case 'F':
+                    break;
+                case 'G':
+                    break;
+                case 'H':
+                    break;
+                case 'I':
+                    break;
+                case 'J':
+                    break;
+                case 'K':
+                    break;
+                case 'L':
+                    break;
+                case 'M':
+                    break;
+                case 'N':
+                    break;
+                case 'O':
+                    break;
+                case 'P':
+                    break;
+                case 'Q':
+                    break;
+                case 'R':
+                    break;
+                case 'S':
+                    break;
+                case 'T':
+                    break;
+                case 'U':
+                    break;
+                case 'V':
+                    break;
+                case 'W':
+                    break;
+                case 'X':
+                    break;
+                case 'Y':
+                    break;
+                case 'Z':
+                    break;
+                default:
+                    System.out.println("invalid input");
+            }
+
+
+        }
+
+        if(word.length()<1){
+            System.out.println("Please enter a three letter word");
+        }
+        if(word.length()>3){
+            System.out.println("Please enter a three letter word");
+        }
     }
-
-
 }
+
+
+
--- src/WordFinder.java
+++ src/WordFinder.java
@@ -1,7 +1,10 @@
 import java.io.*;
 import java.lang.reflect.Array;
 import java.util.*;
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 public class WordFinder{
 
 
@@ -19,11 +22,19 @@ public class WordFinder{
         WordSorter wordSort = new WordSorter(fileName);
         List<Set<String>> allWords=wordSort.getArray();
         answers=allWords.get(((int) word.charAt(0))-65);
+        ListIterator<Set<String>> iter = allWords.listIterator();
+
         for(int i=1;i<word.length();i++){
+
             answers.retainAll(allWords.get(((int) word.charAt(i))-65));
+
         }
+
+
         
     }
+
+
 }
 
 
